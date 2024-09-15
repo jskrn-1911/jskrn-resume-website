90fcecab99c1c1ee0d601ada4f46be5cd328ac54
jassa3206 <jsingh77247@gmail.com>
2024-09-15 21:32:30 +0530

    created structure

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
+import Header from "@/components/Header/index.js";  // Import header
+import Footer from "@/components/Footer/index.js";  // Import footer
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />     {/* Add header here */}
+      <Component {...pageProps} />
+      <Footer />     {/* Add footer here */}
+    </>
+  );
 }
